2ed4b98a79403d3f545a61114c37ff286d27d88c
newkanekibot <93288020+newkanekibot@users.noreply.github.com>
2021-12-01 20:42:28 +0700

    Update music.py

--- program/music.py
+++ program/music.py
@@ -304,7 +304,7 @@ async def stream(c: Client, m: Message):
         return
     try:
         ubot = (await user.get_me()).id
-        b = await c.get_chat_member(chat_id, ubot)
+        b = await c.get_chat_member(chat_id)
         if b.status == "kicked":
             await m.reply_text(
                 f"@{ASSISTANT_NAME} **is banned in group** {m.chat.title}\n\n» **unban the userbot first if you want to use this bot.**"
